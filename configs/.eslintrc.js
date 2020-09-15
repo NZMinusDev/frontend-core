9186269817858f155c27310c567f5d7ad8e43976
NZMinus <developer.n.zaytsev@gmail.com>
2020-09-15 19:31:49 +0300

    feature(eslint: add useful plugins)

--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: [
     // List of recommended rules by https://github.com/iamturns/eslint-config-airbnb-typescript
     "airbnb-base",
+    "plugin:sonarjs/recommended", // detect bugs and suspicious patterns in your code (huge unreadable blocks of code)
+    "plugin:no-use-extend-native/recommended", // prevent use of extended native objects
     /**
      * List of recommended rules for TypeScript from "@typescript-eslint" plugin
      */
@@ -17,4 +19,23 @@ module.exports = {
      */
     "plugin:prettier/recommended",
   ],
+  plugins: ["no-loops", "promise"],
+  rules: {
+    "no-loops/no-loops": 2, // Disallow use of loops (for, for-in, while, do-while, for-of) - we have forEach, map etc.
+    /**
+     * Enforce best practices for JavaScript promises.
+     */
+    "promise/always-return": "error",
+    "promise/no-return-wrap": "error",
+    "promise/param-names": "error",
+    "promise/catch-or-return": "error",
+    "promise/no-native": "off",
+    "promise/no-nesting": "warn",
+    "promise/no-promise-in-callback": "warn",
+    "promise/no-callback-in-promise": "warn",
+    "promise/avoid-new": "warn",
+    "promise/no-new-statics": "error",
+    "promise/no-return-in-finally": "warn",
+    "promise/valid-params": "warn",
+  },
 };
