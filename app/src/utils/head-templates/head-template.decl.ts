acfbb8b4486e785c7647cfcc5fbd602048bd1592
NZMinus <developer.n.zaytsev@gmail.com>
2020-09-17 18:48:24 +0300

    fix(normalize/opinionated)

--- app/src/utils/head-templates/head-template.decl.ts
+++ app/src/utils/head-templates/head-template.decl.ts
@@ -2,4 +2,4 @@
  * Here you should import stuff intended be loading first on your page
  * Tip: It is provided by injecting bundle to head section of page.
  */
-import "./head-template.scss";
+import "./head-template.css";
