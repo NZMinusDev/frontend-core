7e4a85da6408354c22e7a900b90d4cb14d0fa6cc
NZMinus <developer.n.zaytsev@gmail.com>
2021-01-13 21:00:31 +0300

    feat(devTools: add paesers module)

--- app/src/utils/devTools/devTools.ts
+++ app/src/utils/devTools/devTools.ts
@@ -1,3 +1,4 @@
 export * as DOMHelper from "./tools/DOMHelper";
 export * as WindowHrefHelper from "./tools/WindowHrefHelper";
 export * as PluginCreation from "./tools/PluginCreation";
+export * as Parsers from "./tools/Parsers";
